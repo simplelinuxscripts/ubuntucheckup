f73f2919b668391233521bc35bacbe4dc72e8a6d
simplelinuxscripts <simplelinuxscripts@gmail.com>
2025-06-23 23:51:10 +0200

    Add files via upload

--- checkup.sh
+++ checkup.sh
@@ -1065,11 +1065,11 @@ fi
 snap_package_urls=$(echo "$snap_package_info" | grep 'store-url')
 snap_packages_with_unexpected_urls=$(echo "${snap_package_urls}" | grep -v 'https://snapcraft.io/')
 if [ -z "${snap_package_urls}" ]; then
-    print_error "no store-url found in snap packages info (this error can appear if no network connection)"
+    print_warning "no store-url found in snap packages info (this error can appear if no network connection)"
     snap_error_found=1
 elif [ -n "${snap_packages_with_unexpected_urls}" ]; then
     echo "${snap_packages_with_unexpected_urls}"
-    print_error "some snap packages are installed from an unexpected URL (this error can appear if no network connection)"
+    print_error "some snap packages are installed from an unexpected URL"
     snap_error_found=1
 fi
 snap_package_licenses=$(echo "$snap_package_info" | grep 'license:')
